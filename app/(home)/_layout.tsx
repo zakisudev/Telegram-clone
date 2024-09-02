b99af25a4e9036ab9061e8d90b979f056cc099f7
zakisudev <zakisudev@gmail.com>
2024-09-02 16:54:01 +0300

    chore: Update .gitignore and remove .env file

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,45 +1,13 @@
-import { View, Text } from 'react-native';
-import React, {useEffect} from 'react';
-import { Stack, Slot } from 'expo-router';
-import { StreamChat } from 'stream-chat';
-import { OverlayProvider, Chat } from 'stream-chat-expo';
 import ChatProvider from '../../providers/ChatProvider';
-
-const client = StreamChat.getInstance('ts8m6mjqhp4j')
+import { Stack } from 'expo-router';
 
 const HomeLayout = () => {
-
-  useEffect(()=> {
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: 'jlahey',
-          name: 'Jim Lahey',
-          image: 'https://i.imgur.com/fR9Jz14.png',
-        },
-        client.devToken('jlahey')
-      );
-
-      // const channel = client.channel('messaging', 'the_park', {
-      //   name: "The Park"
-      // })
-
-      // await channel.watch();
-    };
-
-    connect()
-  }, [])
-
   return (
-    <OverlayProvider>
-      <Chat client={client}>
-        <ChatProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
-          </Stack>
-        </ChatProvider>
-      </Chat>
-    </OverlayProvider>
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider>
   );
 };
 
--- providers/ChatProvider.tsx
+++ providers/ChatProvider.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Stack, Slot } from 'expo-router';
+import { StreamChat } from 'stream-chat';
+import { OverlayProvider, Chat } from 'stream-chat-expo';
 
-const ChatProvider = ({children}) => {
+const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
+
+const ChatProvider = ({ children }) => {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    const connect = async () => {
+      await client.connectUser(
+        {
+          id: 'jlahey',
+          name: 'Jim Lahey',
+          image: 'https://i.imgur.com/fR9Jz14.png',
+        },
+        client.devToken('jlahey')
+      );
+
+      setIsReady(true);
+
+      // const channel = client.channel('messaging', 'the_park', {
+      //   name: "The Park"
+      // })
+
+      // await channel.watch();
+    };
+
+    connect();
+
+    return () => {
+      client.disconnectUser();
+      setIsReady();
+    };
+  }, []);
+
+  if (!isReady) return <ActivityIndicator />;
+
   return (
-    <>
-      {children}
-    </>
-  )
-}
+    <OverlayProvider>
+      <Chat client={client}>{children}</Chat>
+    </OverlayProvider>
+  );
+};
 
-export default ChatProvider
+export default ChatProvider;
