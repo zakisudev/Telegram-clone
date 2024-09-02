8e67de59845e669ca3c857940f989f5d9c5e2ad8
zakisudev <zakisudev@gmail.com>
2024-09-02 12:50:22 +0300

    chore: Add react-native-gesture-handler plugin and update dependencies, and Stream chat sdk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native';
 import { Stack } from 'expo-router';
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler'
 
 const _layout = () => {
-  return <Stack />;
+  return 
+  <GestureHandlerRootView style={{flex: 1}}>
+    <Stack />;
+  </GestureHandlerRootView>
 };
 
 export default _layout;
